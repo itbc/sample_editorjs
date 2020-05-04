601c7ce4f57181d9de1aab2f41eb1fa7b322431a
itbc <tomo.itbc.web@gmail.com>
2020-05-04 20:16:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sample_editorjs for CDN & node
---
+++ editorjs_node/editorjs_vue/src/components/HelloWorld.vue
@@ -0,0 +1,81 @@
+<template>
+  <div >
+    <div >
+      <h1 style="text-align:center">This is vue editor.js <a href="#" target="_blank">Link</a></h1>
+    </div>
+
+    <div class="editorx_body">
+      <!--editorjs id-->
+      <div class id="codex-editor"/>
+    </div>
+    <button style="margin-left: 30%;" type="button" name="button" @click="save()">save</button>
+    <div class="editorx_body">
+      <pre>{{value}}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import Paragraph from "@editorjs/paragraph";
+import List from "@editorjs/list";
+export default {
+  data() {
+    return {
+      value: null
+    };
+  },
+  methods: {
+    save: function() {
+      editor.save().then(savedData => {
+        console.log(savedData);
+        this.value = savedData;
+      });
+    },
+    myEditor: function() {
+      window.editor = new EditorJS({
+        holder: "codex-editor",
+        autofocus: true,
+        /**
+         * This Tool will be used as default
+         */
+        initialBlock: "paragraph",
+        tools: {
+          header: {
+            class: Header,
+            shortcut: "CMD+SHIFT+H"
+          },
+          list: {
+            class: List
+          },
+          paragraph: {
+            class: Paragraph,
+            config: {
+              placeholder: "."
+            }
+          }
+        },
+        onReady: function() {
+          console.log("ready");
+        },
+        onChange: function() {
+          console.log("change");
+        }
+      });
+    }
+  },
+  mounted: function() {
+    this.myEditor();
+  }
+};
+</script>
+
+<style lang="css" scoped >
+.editorx_body {
+  width: 60%;
+  margin-left: 20%;
+  border: 2px solid #f1f3f5;
+  box-sizing: border-box;
+}
+</style>
